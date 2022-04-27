293ad9996d343db7bcb1fae7d910f52844b84977
Manel <manelhammouche3@gmail.com>
2022-04-27 15:55:33 +0100

    Add status class to handle tasks status

--- src/status.js
+++ src/status.js
@@ -1,6 +1,30 @@
-import Tasks from './tasks.js';
+export default class Status {
+  constructor() {
+    this.tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+  }
 
-const tasks = new Tasks();
+  // complete tasks and update task status
+  completeTask = () => {
+    this.tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+    const checkboxes = document.querySelectorAll('.check-task');
+    const todoTask = document.querySelectorAll('.todo-task');
+    checkboxes.forEach((checkbox, index) => {
+      checkbox.addEventListener('click', () => {
+        todoTask[index].classList.toggle('active');
+        this.tasksArray[index].isCompleted = !this.tasksArray[index].isCompleted;
+        localStorage.setItem('tasks', JSON.stringify(this.tasksArray));
+      });
+    });
+  }
 
-// complete tasks
-const checkbox = document.querySelectorAll('.check-task');
+  // clear the completed tasks
+  clearCompleted = () => {
+    const clearBtn = document.querySelector('#clear-completed');
+    clearBtn.addEventListener('click', () => {
+      this.tasksArray = JSON.parse(localStorage.getItem('tasks')) || [];
+      this.tasksArray = this.tasksArray.filter((item) => item.isCompleted === false);
+      localStorage.setItem('tasks', JSON.stringify(this.tasksArray));
+      document.location.reload();
+    });
+  }
+}
